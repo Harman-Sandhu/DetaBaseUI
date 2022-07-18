7a987809061a34c084e50f0fbbe828bd4b56caa0
Harman Sandhu <35080189+Harman-Sandhu@users.noreply.github.com>
2022-07-18 01:02:36 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ export default function Home() {
   const [popupContent, setPopupContent] = useState({})
   const [keys, setKeys] = useState()
 
-  // detect click outside popup
   useEffect(() => {
     const handleClickOutside = (e) => {
       if (popupOpened && e.target.classList.contains('popup')) {
@@ -36,23 +35,18 @@ export default function Home() {
     }
     try {
       const deta = Deta(projectKey)
-      // console.log('init', projectKey, detaBaseName)
       const db = deta.Base(baseName)
       const all = await db.fetch()
-      // get all keys from multiple levels without duplicates and remove "key"
       const keys = all.items.reduce((acc, item) => {
-        // no duplicate keys and remove "key"
         return [...acc, ...Object.keys(item).filter(key => key !== 'key')]
       }
         , [])
-      // remove duplicate keys and sort alphabetically
       var unique = keys.filter((item, index) => keys.indexOf(item) === index).sort()
       setKeys(unique)
       setDetaBaseContent(all.items)
     } catch (error) {
       alert(error)
     }
-    // console.log(all)
   }
 
   const changeArrayEmpty = () => {
@@ -65,22 +59,18 @@ export default function Home() {
   }
 
   const handleSelect = (e, detaRow) => {
-    // console.log(e.target.checked, e.target.id, isShiftKeyDown, detaRow, lastSelected)
     if (e.target.checked && lastSelected != null && isShiftKeyDown) {
       for (let i = lastSelected; i != e.target.id; (i <= e.target.id ? i++ : i--)) {
         if (i == lastSelected) continue;
         const el = document.getElementById(i)
         if (el) {
-          // console.log(detaBaseContent[i], i)
           selectedKeys.push(detaBaseContent[i].key)
           el.checked = true
-          // console.log(i, e.target.id)
         }
       }
       selectedKeys.push(detaBaseContent[e.target.id].key)
     } else if (isShiftKeyDown === false) {
       if (e.target.checked) {
-        // console.log(detaBaseContent[e.target.id], e.target.id)
         selectedKeys.push(detaBaseContent[e.target.id].key)
       }
     }
@@ -90,32 +80,25 @@ export default function Home() {
         const el = document.getElementById(i)
         if (el) {
           var done = selectedKeys.splice(selectedKeys.indexOf(detaBaseContent[i].key), 1)
-          // console.log(done, selectedKeys)
           el.checked = false
-          // console.log(i, e.target.id)
         }
       }
     }
 
     if (!e.target.checked) {
-      // remove from selectedKeys if it is there
-      // console.log(detaBaseContent)
       selectedKeys.splice(selectedKeys.indexOf(detaRow), 1)
 
     }
     setLastSelected(parseInt(e.target.id))
     changeArrayEmpty()
-    // console.log(selectedKeys)
   }
 
   const deleteItem = async () => {
     if (selectedKeys.length > 0) {
       const deta = Deta(projectKey)
       const db = deta.Base(detaBaseName)
-      // delete all selected items
 
       for (let i = selectedKeys.length - 1; i >= 0; i--) {
-        // uncheck the checkbox
         const el = document.getElementById(i)
         if (el) {
           el.checked = false
@@ -125,12 +108,9 @@ export default function Home() {
         if (key) {
           await db.delete(key)
         }
-        // remove from selectedKeys if it is there
         selectedKeys.splice(selectedKeys.indexOf(selectedKeys[i]), 1)
-        // i-=1
       }
       changeArrayEmpty()
-      // console.log('deleted')
       init()
     }
   }
@@ -145,9 +125,7 @@ export default function Home() {
   const updateItem = async () => {
     const deta = Deta(projectKey)
     const db = deta.Base(detaBaseName)
-    // const res = await db.update(popupContent.item.key, { [popupContent.key]: popupContent.value })
     await db.update({ [popupContent.key]: (popupContent.value[0] === "{" ? (popupContent.value.length > 1 ? JSON.parse(popupContent.value) : JSON.parse(popupContent.value)) : popupContent.value[0] === "[" ? (popupContent.value.length > 1 ? JSON.parse(popupContent.value) : null) : popupContent.value) }, popupContent.item.key)
-    // console.log(popupContent)
     setPopupOpened(false)
 
     init()
@@ -159,13 +137,12 @@ export default function Home() {
         <title>{`DETA Base UI ` + (detaBaseName && "- " + detaBaseName)}</title>
         <link rel="icon" href="/favicon.svg" />
 
-        {/* meta tags */}
         <meta name="title" content="DETA Base UI" />
         <meta
           name="description"
           content="A place with more functionality for managing your Deta Base(s)."
         />
-        {/* Open Graph / Facebook */}
+
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://harmansandhu.tech/baseui" />
         <meta property="og:title" content="DETA Base UI" />
@@ -174,7 +151,7 @@ export default function Home() {
           content="A place with more functionality for managing your Deta Base(s)."
         />
         <meta property="og:image" content="https://harmansandhu.tech/BaseUI.jpg" />
-        {/* Twitter */}
+
         <meta property="twitter:card" content="https://harmansandhu.tech/BaseUI.jpg" />
         <meta property="twitter:url" content="https://harmansandhu.tech/baseui" />
         <meta property="twitter:title" content="DETA Base UI" />
@@ -188,7 +165,7 @@ export default function Home() {
       <div className={"py-5" + (popupOpened ? " blur-[2px] py-8 animate-pulse" : "")}>
         <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row mb-1 sm:mb-0 justify-between w-full">
           <div className="flex  md:justify-center my-auto items-center max-h-6">
-            {/* <img src="https://docs.deta.sh/img/logo.svg" alt="DETA" className="h-10 w-10" /> */}
+
             <div className="text-2xl leading-tight" >
               Deta Base
               <span>
@@ -209,7 +186,6 @@ export default function Home() {
                 }
               </span>
             </div>
-            {/* total rows */}
             <div className="text-sm self-end text-gray-600">
               {detaBaseContent.length > 0 &&
                 <>
@@ -220,7 +196,6 @@ export default function Home() {
           </div>
           {detaBaseContent.length > 0 &&
             <div className="flex flex-row justify-end w-full md:w-auto space-x-1 justify-items-end content-end md:items-center">
-              {/* add button */}
               <button
                 className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-3 rounded"
                 onClick={addItem}
@@ -230,7 +205,6 @@ export default function Home() {
                 </svg>
               </button>
 
-              {/* delete button */}
               {!isEmpty && (
                 <button
                   className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
@@ -249,7 +223,6 @@ export default function Home() {
                 </div>
               </form>
 
-              {/* refresh button - bold svg*/}
               <button
                 className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-3 rounded"
                 onClick={init} title="Fetch">
@@ -267,19 +240,13 @@ export default function Home() {
               <table className="min-w-full leading-normal">
                 <thead className="z-50 sticky top-0">
                   <tr>
-                    {/* empty heading for select button */}
                     <th className="px-4 py-2 bg-gray-100 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                      {/* <label title='Select' className="flex items-center justify-center space-x-3">
-                        <input type="checkbox" className="form-tick hover:cursor-pointer appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-pink-500 checked:border-transparent focus:outline-none" />
-                      </label> */}
                       ⠀
                     </th>
-                    {/* get all these: key, image, name, price, prodId, user from detaBaseContent*/}
                     <th className="px-4 py-2 bg-gray-100 text-xs text-center leading-4 font-medium text-gray-500 uppercase tracking-wider">
                       key
                     </th>
                     {
-                      // get all from "keys" variable and null if is "key"
                       keys.map((key, index) => {
                         if (key !== "key") {
                           return (
@@ -294,16 +261,12 @@ export default function Home() {
                 </thead>
                 <tbody>
                   {detaBaseContent.map((item, index) => {
-                    {/* select button for each row in detaBaseContent*/ }
                     return <React.Fragment key={index}>
                       {item.key.includes(Search) &&
                         <tr key={index + "key"}>
                           <td className="md:min-w-[72px] w-[72px] md:max-w-[72px] py-2 bg-gray-100">
                             <label className="flex items-center  justify-center flex-col relative ">
                               <input onKeyUp={(e) => { setIsShiftKeyDown(e.shiftKey) }} onChange={(e) => { handleSelect(e, item.key) }} onKeyDown={(e) => { setIsShiftKeyDown(e.shiftKey) }} id={index} type="checkbox" className="relative hover:cursor-pointer text-pink-500 bg-white h-6 w-6 border border-gray-300 rounded-md focus:ring-pink-500 focus:outline-none" />
-                              {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-white absolute " fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                              <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-                            </svg> */}
                             </label>
                           </td>
                           <td className="md:px-5 py-5 border-t border-r border-gray-200 bg-white px-2 ">
@@ -315,7 +278,6 @@ export default function Home() {
                               </div>
                             </div>
                           </td>
-                          {/* put the item in the correct slot and null if its key */}
                           {keys.map((key, index) => {
                             if (key !== "key") {
                               return (
@@ -386,19 +348,11 @@ export default function Home() {
           </>
         }
       </div>
-      {/* small editable popup on center of the page with save and close button */}
       {popupOpened &&
         <div className="fixed inset-0 z-50 overflow-auto">
           <div className="flex items-center justify-center min-h-screen popup">
             <div className="max-w-lg w-full bg-white rounded-3xl shadow-lg overflow-y-auto">
               <div className="relative z-10 px-4 py-5 sm:p-6">
-                {/* <div className="flex justify-end">
-                  <button onClick={() => { setPopupOpened(false) }} className="text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-300 focus:shadow-outline-gray transition ease-in duration-150">
-                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                  </button>
-                </div> */}
                 <div className={`relative flex flex-col ` + ((typeof popupContent.value === "boolean") ? "min-h-[15vh]" : "min-h-[35vh]")} >
                   {typeof popupContent.value == "boolean" ?
                     <>
@@ -427,7 +381,6 @@ export default function Home() {
                       }} readOnly={false} defaultValue={Array.isArray(popupContent.value) ? (
                         JSON.stringify(popupContent.value, null, 2)
                       ) : typeof popupContent.value === 'object' ? JSON.stringify(popupContent.value, null, 2) : popupContent.value} className="w-full max-h-56 flex-grow min-h-16 bg-slate-100 focus:border-pink-500 focus:ring-pink-500 border-2 border-gray-300 rounded-lg shadow-sm p-4 text-gray-700 resize-none" placeholder="Enter your content here" />
-                      {/* {console.log(typeof popupContent.value)} */}
                       <button onClick={() => { updateItem() }}
                         className="rounded-md py-1 mt-2 bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2">
                         Save
